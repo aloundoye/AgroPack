171295ef5bc864f021f8beb110f2446a171b3139
Alassane Ndoye <aloutndoye.an@gmail.com>
2020-08-04 12:41:32 +0000

    Repository pattern

---
+++ AgroPack/Repository/GenericUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgroPack.Repository
+{
+    public class GenericUnitOfWork : IDisposable
+    {
+        private AgroPackDbContext DBEntity = new AgroPackDbContext();
+        public IRepository<Tbl_EntityType> GetRepositoryInstance<Tbl_EntityType>() where Tbl_EntityType : class
+        {
+            return new GenericRepository<Tbl_EntityType>(DBEntity);
+        }
+
+        public void SaveChanges()
+        {
+            DBEntity.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    DBEntity.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool disposed = false;
+    }
+}
