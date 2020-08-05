8ba6e90bdb5a52b43bc30712a53ffd1525facb07
Alassane Ndoye <aloutndoye.an@gmail.com>
2020-08-05 00:23:57 +0000

    Ajout des differentes pages dans le tableau de bord

--- AgroPack/Controllers/DashboardController.cs
+++ AgroPack/Controllers/DashboardController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AgroPack.Repository;
 
 namespace AgroPack.Controllers
 {
     public class DashboardController : Controller
     {
+        public GenericUnitOfWork _UnitOfWork = new GenericUnitOfWork();
         // GET: Dashboard
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Categorie> allCategories =
+                _UnitOfWork.GetRepositoryInstance<Categorie>().GetAllRecordsIQueryable().ToList();
+            return View(allCategories);
         }
     }
 }
