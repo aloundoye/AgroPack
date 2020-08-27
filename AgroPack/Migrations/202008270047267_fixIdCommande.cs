c28027bfb51815df87991ce8a4f68a2272d95589
Alassane Ndoye <aloutndoye.an@gmail.com>
2020-08-27 10:41:39 +0000

    fixIdCommande

---
+++ AgroPack/Migrations/202008270047267_fixIdCommande.cs
@@ -0,0 +1,16 @@
+﻿namespace AgroPack.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixIdCommande : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
